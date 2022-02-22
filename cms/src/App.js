520b3461461a7a1302b4c23584751da3af031b3c
sanjeet-tech-radix <98081464+sanjeet-tech-radix@users.noreply.github.com>
2022-02-22 17:06:24 +0530

    added basic component

---
+++ cms/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Header from './screens/admin-screen/common/header';
+function App() {
+  return (
+   <React.Fragment>
+    <Header/>
+     </React.Fragment>
+  );
+}
+
+export default App;
